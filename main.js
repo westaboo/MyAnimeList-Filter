70b4ecebf8f26a30e70731c52d2c1ad598490f9a
Zeeshan Hanif <zhanif@uci.edu>
2016-12-05 20:18:37 -0800

    now in separate functions

--- main.js
+++ main.js
@@ -12,37 +12,46 @@ to_remove.add('Haikyuu!!');
 
 ///////////////////////////////////////////////////////////////
 
-// Removes elements whose titles match from the set made above
-
 var anime_list = document.getElementsByClassName('ranking-list');
 
-for (var i = anime_list.length - 1; i >= 0; i--)
-{   
-    var elementName = anime_list[i].getElementsByClassName('fw-b')[0].innerHTML;
-    
-    for (let anime of to_remove)
-    {
-        if (elementName.includes(anime))
+filter(to_remove);
+renumber();
+
+// Removes elements whose titles match from the set made above
+
+function filter(to_remove) {
+
+    for (var i = anime_list.length - 1; i >= 0; i--)
+    {   
+        var elementName = anime_list[i].getElementsByClassName('fw-b')[0].innerHTML;
+        
+        for (let anime of to_remove)
         {
+            if (elementName.includes(anime))
+            {
                 anime_list[i].remove();
                 break;
+            }
         }
     }
 }
 
+
 // Gets starting number to start numbering from
 
-var baseURL = anime_list[0].getElementsByClassName('fw-b')[0].baseURI;
-var limitLocation = baseURL.search('limit=');
-var beginning = 0;
+function renumber() {
 
-if (limitLocation != -1)
-    beginning = parseInt(baseURL.substr(limitLocation + 6));
+    var limitLocation = document.URL.search('limit=');
+    var beginning = 0;
 
-// Renumbers all anime
+    if (limitLocation != -1)
+        beginning = parseInt(document.URL.substr(limitLocation + 6));
 
-for (var i = 0; i < anime_list.length; i++)
-{
-    var rank = anime_list[i].getElementsByClassName('top-anime-rank-text')[0];
-    rank.innerHTML = i + 1 + beginning;
+    // Renumbers all anime
+
+    for (var i = 0; i < anime_list.length; i++)
+    {
+        var rank = anime_list[i].getElementsByClassName('top-anime-rank-text')[0];
+        rank.innerHTML = i + 1 + beginning;
+    }
 }
